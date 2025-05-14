33414c10447e5e5939bb6508fc6213c54b333869
riffi023 <kazukiakumaru023@gmail.com>
2025-05-14 14:07:25 +0700

    save

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+app.get('/',(req, res) => {
+  res.send('hello world!');
+});
 app.use("/users", require("./routes/userroute"));
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
